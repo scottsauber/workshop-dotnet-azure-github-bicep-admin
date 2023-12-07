0778bcaebf81f9b0af5b8dbd9c1e4c633b47b6d2
scottsauber <stsauber@gmail.com>
2023-12-06 22:51:00 -0600

    add azure pinger

---
+++ AzurePinger/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var httpClient = new HttpClient();
+while (true)
+{
+    var result = await httpClient.GetStringAsync("https://app-workshop-dnazghbicep-dev.azurewebsites.net/api/version");
+    Console.WriteLine($"{DateTime.Now:h:mm:ss tt} - {result}");
+    await Task.Delay(1_000);
+}
